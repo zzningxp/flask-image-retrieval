5d69f8b5fb82ee50523d66fac94c18723e9234b0
zzningxp <zzningxp@gmail.com>
2016-07-11 23:24:49 +0800

    hamming code distance

--- imagesearch/image_retrieval.py
+++ imagesearch/image_retrieval.py
@@ -12,22 +12,34 @@ class Retriever(object):
         self.qf = h5py.File('db/'+dir+'/db_feat4096.h5')
         self.nf = h5py.File('db/'+dir+'/db_filename.h5')
         self.queryDB = np.array(self.qf['feat_db'])
+        self.signDB = np.sign(self.queryDB)
         self.path = np.array(self.nf['list_im']).flatten()
         if 'cate_im' in self.nf:
             self.cate = np.array(self.nf['cate_im']).flatten()
         else:
             self.cate = []
     
+    def euclidean_dist(self, queryImg):
+        queryImg = preprocessing.normalize(queryImg, norm='l2')
+        dists = cdist(queryImg, self.queryDB, 'euclidean')
+        return np.argsort(dists).flatten()
+
+    def hamming_dist(self, queryImg):
+        q = np.sign(queryImg)
+        res = []
+        for i, x in enumerate(self.signDB):
+            res.append((i, np.count_nonzero(x==q)))
+        res.sort(key=lambda tup: tup[1], reverse=True)
+        ind = [x[0] for x in res]
+        return ind
+
     def retrieval(self, img_name):
         tic = time.time()
 
         queryImg = pd.read_csv('%s.csv'%img_name,header=None).as_matrix()
-        queryImg = preprocessing.normalize(queryImg, norm='l2')
     
-        #print queryDB, queryDB.shape
-        #print queryImg, queryImg.shape
-        dists = cdist(queryImg, self.queryDB, 'euclidean')
-        ind = np.argsort(dists).flatten()
+        #ind = self.euclidean_dist(queryImg)
+        ind = self.hamming_dist(queryImg)
     
         ##print dists, ind, path, path.shape
         k = 100
